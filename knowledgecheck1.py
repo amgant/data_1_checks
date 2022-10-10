854c71df6eac47dae1801c305dbef2e4b5573904
amgant <115430681+amgant@users.noreply.github.com>
2022-10-09 21:30:16 -0400

    Add files via upload
    Just import the file name to your terminal and it will go. :)

---
+++ knowledgecheck1.py
@@ -0,0 +1,25 @@
+hello = print("hello world!")
+
+
+
+list_numbers = ["Uno", 2, "tres", 4.9]
+print(list_numbers)
+print(list_numbers[2])
+
+
+
+dictionary_volume = {'Pi': 3.14,'seasonal depression': 100}
+print(dictionary_volume)
+print("The volume of my seasonal depression is already on",
+      dictionary_volume["seasonal depression"]
+     )
+
+
+
+tuple_snl = ("Maya Rudolph", "Kate Mckinnon", "Melissa Villasenor", "Kristin Wiig")
+print(tuple_snl)
+print(tuple_snl[3])
+
+
+
+goodbye = print("thank you for your time! :)")
